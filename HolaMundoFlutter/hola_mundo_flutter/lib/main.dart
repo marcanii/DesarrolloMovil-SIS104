2716b8f7aa45c37fe7e1761d605df05f77db1c8b
marcanii <israelmarcani10@gmail.com>
2024-05-23 09:31:36 -0400

    clase principal Stateless

--- HolaMundoFlutter/hola_mundo_flutter/lib/main.dart
+++ HolaMundoFlutter/hola_mundo_flutter/lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Principal());
+}
+
+class Principal extends StatelessWidget {
+  const Principal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Saludo'),
+        ),
+        body: const Center(
+          child: MiClaseEstatica(),
+        ),
+      ),
+    );
+  }
+}
+
+class MiClaseEstatica extends StatelessWidget {
+  const MiClaseEstatica({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hola Mundo!',
+        style: TextStyle(
+            fontSize: 24, color: Colors.blue, fontWeight: FontWeight.bold));
+  }
 }
 
 class MainApp extends StatelessWidget {
